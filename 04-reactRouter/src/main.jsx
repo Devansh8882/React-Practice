4ba80686bcd982e91b8913d8fc8c846b07c33398
devanshm <devansh.m@sarv.com>
2025-02-27 17:51:55 +0530

    new practice routers in react

--- 04-reactRouter/src/main.jsx
+++ 04-reactRouter/src/main.jsx
@@ -1,34 +1,53 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {createBrowserRouter,RouterProvider} from "react-router-dom";
+import {createBrowserRouter,RouterProvider,createRoutesFromElements , Route} from "react-router-dom";
 import './index.css'
 // import App from './App.jsx'
 import Header from './components/header/header.jsx';
 import Footer from './components/footer/footer.jsx';
 import Home from './components/home/home.jsx';
 import Layout from './layout.jsx';
-const router = createBrowserRouter([
-  {
-    path:"/",
-    element : <Layout/>,
-    children:[
-      {
-        path:"",
-        element:<Home/>
-      },
-      {
-        path:"header",
-        element:<Header/>
-      },
-      {
-        path:"foot",
-        element:<Footer/>
-      },
-      
-    ]
-  }
-])
 
+
+// const router = createBrowserRouter([
+//   {
+//     path:"/",
+//     element : <Layout/>,
+//     children:[
+//       {
+//         path:"",
+//         element:<Home/>
+//       },
+//       {
+//         path:"header",
+//         element:<Header/>
+//       },
+//       {
+//         path:"foot",
+//         element:<Footer/>
+//       },
+      
+//     ]
+//   }
+// ])
+
+//  we can do the same thing another way also....
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = "" element ={<Layout />}>
+        <Route path = "" element ={<Home />} />
+        <Route path = "header" element ={<Header />} />
+        <Route path = "foot" element ={<Footer />} />
+    </Route>
+    // <Router>
+   
+    // </Router>
+  )
+)
+
+
+//.........
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
